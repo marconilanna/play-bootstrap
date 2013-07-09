217e109e603b02fa6488582efb080f36dde12431
Marconi Lanna <marconi.lanna@gmail.com>
2013-07-09 12:07:45 -0400

    Play 2.1.2 & Scala 2.10.2

--- project/Build.scala
+++ project/Build.scala
@@ -16,7 +16,7 @@ object ApplicationBuild extends Build {
 		)
 
 	val main = play.Project(appName, appVersion, appDependencies).settings(
-		  scalaVersion := "2.10.1"
+		  scalaVersion := "2.10.2"
 		, scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature", "-Xlint")
 		, templatesImport ++= Seq("helpers._", "tags._", "util._")
 		, testOptions in Test := Nil // Disables built-in specs2, it conflicts with ScalaTest
